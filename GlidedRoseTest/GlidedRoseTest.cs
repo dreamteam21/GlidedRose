8e565d2f376bbb803229cd2ec91cee98071ee2bc
Dream <phattharapong.poolthong@gmail.com>
2022-08-26 13:42:48 +0700

    Add Backstage passes item and create test cases for it

--- GlidedRoseTest/GlidedRoseTest.cs
+++ GlidedRoseTest/GlidedRoseTest.cs
@@ -86,7 +86,44 @@ public class GlidedRoseTest
     }
 
     [TestCase(11,5,6)]
-    public void ShouldBackstagePassesIncreaseBy1WhenSellInMoreThan10(int sellIn, int quality, int expected_quality)
+    [TestCase(14,31,32)]
+    public void ShouldBackstagePassIncreaseBy1WhenSellInMoreThan10Days(int sellIn, int quality, int expected_quality)
+    {
+        BackstagePasses backstagePasses = new BackstagePasses();
+        backstagePasses.SellIn = sellIn;
+        backstagePasses.Quality = quality;
+        backstagePasses.UpdateItem();
+        Assert.That(backstagePasses.Quality, Is.EqualTo(expected_quality));
+    }
+    
+    [TestCase(8,9,11)]
+    [TestCase(6,30,32)]
+    [TestCase(10,49,50)]
+    public void ShouldBackstagePassIncreaseBy2WhenSellInBetween6To10Days(int sellIn, int quality, int expected_quality)
+    {
+        BackstagePasses backstagePasses = new BackstagePasses();
+        backstagePasses.SellIn = sellIn;
+        backstagePasses.Quality = quality;
+        backstagePasses.UpdateItem();
+        Assert.That(backstagePasses.Quality, Is.EqualTo(expected_quality));
+    }
+    
+    [TestCase(5,13,16)]
+    [TestCase(4,4,7)]
+    [TestCase(3,48,50)]
+    public void ShouldBackstagePassIncreaseBy3WhenSellInLessThan6Days(int sellIn, int quality, int expected_quality)
+    {
+        BackstagePasses backstagePasses = new BackstagePasses();
+        backstagePasses.SellIn = sellIn;
+        backstagePasses.Quality = quality;
+        backstagePasses.UpdateItem();
+        Assert.That(backstagePasses.Quality, Is.EqualTo(expected_quality));
+    }
+    
+    [TestCase(0,13,0)]
+    [TestCase(-10,4,0)]
+    [TestCase(-5,48,0)]
+    public void ShouldBackstagePassBe0AfterTheConcert(int sellIn, int quality, int expected_quality)
     {
         BackstagePasses backstagePasses = new BackstagePasses();
         backstagePasses.SellIn = sellIn;
